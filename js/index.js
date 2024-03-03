a121bd7429df25ddd15eaafa5252d25e87f9fe5c
Geider10 <geiderde20@gmail.com>
2024-03-03 18:46:32 -0300

    cree las lista y las tares del tablero y le puse estilo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log("aguante Boca");
+const d = document;
+const txtArea = d.getElementById("area");
+txtArea.addEventListener("keyup",e=>{
+    txtArea.style.height = "54px";
+    let scHeight = e.target.scrollHeight;
+    txtArea.style.height = `${scHeight}px`;
+})
